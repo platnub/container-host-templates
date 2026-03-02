f80f5c11d28f8d866644be725190f200977c5d60
platnub <81484914+platnub@users.noreply.github.com>
2026-03-02 14:25:20 +0100

    Refactor SSH key setup script for clarity

--- virtual-machines/docker/ssh.sh
+++ virtual-machines/docker/ssh.sh
@@ -4,33 +4,18 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
+echo "Starting SSH key setup..."
+ssh-keygen -t rsa
+cat ~/.ssh/rd_rsa
+read -p "Copy the FULL private key, import into Bitwarden, then press ENTER to continue..."
+read -r -p "Paste your public key (single line): " PUBKEY
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
 
-read -p "Are you running this script as root? [Y/n]: " answer
-
-case "$answer" in
-    [Yy]* )
-        echo "Script needs to be ran as user."
-        exit 1
-        ;;
-    [Nn]* )
-        echo "Starting SSH key setup..."
-        ssh-keygen -t rsa
-        cat ~/.ssh/rd_rsa
-        read -p "Copy the FULL private key, import into Bitwarden, then press ENTER to continue..."
-        read -r -p "Paste your public key (single line): " PUBKEY
-        touch ~/.ssh/authorized_keys
-        chmod 600 ~/.ssh/authorized_keys
-
-        # Check if key already exists
-        if ! grep -qxF "$PUBKEY" ~/.ssh/authorized_keys; then
-          printf '%s\n' "$PUBKEY" >> ~/.ssh/authorized_keys
-          echo "Key added to ~/.ssh/authorized_keys"
-        else
-          echo "Key already exists in ~/.ssh/authorized_keys"
-        fi
-
-        ;;
-    * )
-        echo "Invalid input. Please enter [Y/n]"
-        ;;
-esac
+# Check if key already exists
+if ! grep -qxF "$PUBKEY" ~/.ssh/authorized_keys; then
+  printf '%s\n' "$PUBKEY" >> ~/.ssh/authorized_keys
+  echo "Key added to ~/.ssh/authorized_keys"
+else
+  echo "Key already exists in ~/.ssh/authorized_keys"
+fi
