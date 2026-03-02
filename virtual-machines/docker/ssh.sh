287d5bdcd0f7b49bf68f2eed9b09cea6316beb27
platnub <81484914+platnub@users.noreply.github.com>
2026-03-02 14:20:47 +0100

    Add SSH key setup script

---
+++ virtual-machines/docker/ssh.sh
@@ -0,0 +1,29 @@
+read -p "Are you running this script as root? [Y/n]: " answer
+
+case "$answer" in
+    [Yy]* )
+        echo "Script needs to be ran as user."
+        exit 1
+        ;;
+    [Nn]* )
+        echo "Starting SSH key setup..."
+        ssh-keygen -t rsa
+        cat ~/.ssh/rd_rsa
+        read -p "Copy the FULL private key, import into Bitwarden, then press ENTER to continue..."
+        read -r -p "Paste your public key (single line): " PUBKEY
+        touch ~/.ssh/authorized_keys
+        chmod 600 ~/.ssh/authorized_keys
+
+        # Check if key already exists
+        if ! grep -qxF "$PUBKEY" ~/.ssh/authorized_keys; then
+          printf '%s\n' "$PUBKEY" >> ~/.ssh/authorized_keys
+          echo "Key added to ~/.ssh/authorized_keys"
+        else
+          echo "Key already exists in ~/.ssh/authorized_keys"
+        fi
+
+        ;;
+    * )
+        echo "Invalid input. Please enter [Y/n]"
+        ;;
+esac
