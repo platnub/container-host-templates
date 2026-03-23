ca0bc2d13f6b4d21085fda4089ed7a7c6d409141
platnub <81484914+platnub@users.noreply.github.com>
2026-03-23 20:45:53 +0100

    Update setup.sh

--- docker/scripts/setup.sh
+++ docker/scripts/setup.sh
@@ -70,7 +70,7 @@ groupadd -g 1000 user
 adduser --gecos GECOS --disabled-password --system --uid 1000 --gid 1000 user
 # Configure bkup user
 groupadd -g 1001 bkup
-adduser --gecos GECOS --disabled-password --uid 1001 --gid 1001 bkup
+adduser --gecos GECOS --disabled-password --system --uid 1001 --gid 1001 bkup
 # Configure komodo user
 groupadd -g 1337 komodo
 adduser --gecos GECOS --disabled-password --uid 1337 --gid 1337
